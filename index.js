30109a8d0ee15e6b874ea5aa37fe0bece8b2dd61
AJ Balash <skipptekk@gmail.com>
2020-06-05 07:00:08 -0600

    Added ships.pug and the navbar.

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 3000;
 const app = express();
+const session = require('express-session');
+const TWO_HOURS = 1000 * 60 * 5
+
+const {
+  PORT = 3000,
+  NODE_ENV = 'development',
+  SESS_SECRET = 'qwertzxcbasdfgyuiopnmhjkl',
+
+  SESS_NAME = 'sid',
+  SESS_LIFETIME = TWO_HOURS
+} = process.env
+
+const IN_PROD = NODE_ENV ==='production'
+
+
+
+app.use(session({
+  name: SESS_NAME,
+  resave: false,
+  saveUninitialized: false,
+  secret: SESS_SECRET,
+  cookie: {
+    maxAge: SESS_LIFETIME,
+    sameSite: true,
+    secure: IN_PROD
+  }
+}))
+
 app.set('view engine', 'pug')
 app.use(express.static('public'));
 
@@ -10,7 +37,7 @@ app.get('/', function (req, res) {
   res.render(__dirname + '/public/index')
 });
 app.get('/ships', function (req, res) {
-  res.render(__dirname + '/public/ships.pug')
+  res.render(__dirname + '/public/ships')
 });
 
 //api shit 
